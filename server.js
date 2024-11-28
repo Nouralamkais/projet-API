f86f249b9241f3b74d812804b5fa1f05fb87dcb9
Nouralamkais <nouralamkais22@gmail.com>
2024-11-28 20:53:44 +0100

    Premier commit de mon projet

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//connexion a base de donnee 'mongodb'
+mongoose
+  .connect("mongodb://localhost:27017/employed", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+
+// definition du schema pour les employes
+const employeeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  position: { type: String, required: true },
+  department: { type: String, required: true },
+  salary: { type: Number, required: true },
+});
+
+// modele basé sur le schema
+const Employee = mongoose.model("Employee", employeeSchema);
+
+// route pour recuperer tous les employes
+app.get("/api/employees", async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.json(employees);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Demarrage du serveur
+app.listen(port, () => console.log(`Server running on port ${port}`));
